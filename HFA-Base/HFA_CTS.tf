625f0397664634b346738e47a35613f8c70c1245
agoodmu <agoodmu@gmail.com>
2023-08-16 08:57:29 +0800

    Update HFA_CTS.tf

--- HFA-Base/HFA_CTS.tf
+++ HFA-Base/HFA_CTS.tf
@@ -1,23 +1,28 @@
 resource "huaweicloud_cts_tracker" "hfa_security_account_system_tracker" {
   provider = huaweicloud.security
   bucket_name = var.HFA_Security_OBS_Bucket_Name
+  depends_on = [ huaweicloud_obs_bucket_acl.hfa_cts_log ]
 }
 
 resource "huaweicloud_cts_tracker" "hfa_app_account_system_tracker" {
   provider = huaweicloud.app
   bucket_name = var.HFA_Security_OBS_Bucket_Name
+  depends_on = [ huaweicloud_obs_bucket_acl.hfa_cts_log ]
 }
 
 resource "huaweicloud_cts_tracker" "hfa_common_account_system_tracker" {
   provider = huaweicloud.common
   bucket_name = var.HFA_Security_OBS_Bucket_Name
+  depends_on = [ huaweicloud_obs_bucket_acl.hfa_cts_log ]
 }
 
 resource "huaweicloud_cts_tracker" "hfa_transit_account_system_tracker" {
   provider = huaweicloud.transit
   bucket_name = var.HFA_Security_OBS_Bucket_Name
+  depends_on = [ huaweicloud_obs_bucket_acl.hfa_cts_log ]
 }
 
 resource "huaweicloud_cts_tracker" "hfa_iam_account_system_tracker" {
   bucket_name = var.HFA_Security_OBS_Bucket_Name
+  depends_on = [ huaweicloud_obs_bucket_acl.hfa_cts_log ]
 }
