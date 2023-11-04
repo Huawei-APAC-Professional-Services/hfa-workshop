4b95081d87d61286540016e2deeb23738c1bea6d
mu <agoodmu@gmail.com>
2023-11-04 21:11:47 +0800

    delete module from this repo

--- modules/cts/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "hfa_cts_notification_email_address" {
-  type = string
-}
-
-variable "hfa_security_obs_bucket_name" {
-  type = string
-}
--- modules/vpc/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "vpc_id" {
-    value = huaweicloud_vpc.this.id
-}
